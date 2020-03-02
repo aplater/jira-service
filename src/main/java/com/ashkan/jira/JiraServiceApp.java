bdbd70a6b22ffbfc72ad5de7c4c2c703b764e7e4
ashkanent <ashkan.ent@gmail.com>
2020-03-02 09:53:49 -0500

    Initial commit: setup gradle project with spring boot

---
+++ src/main/java/com/ashkan/jira/JiraServiceApp.java
@@ -0,0 +1,11 @@
+package com.ashkan.jira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraServiceApp {
+	public static void main(String[] args) {
+		SpringApplication.run(JiraServiceApp.class, args);
+	}
+}
