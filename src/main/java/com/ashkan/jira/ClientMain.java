c722eb2b9f0c4b95e5f052f85301f3ca429e843a
Mustafa Haddara <mhaddara@venacorp.com>
2020-05-05 13:16:13 -0400

    add it to the initialization step

--- src/main/java/com/ashkan/jira/ClientMain.java
+++ src/main/java/com/ashkan/jira/ClientMain.java
@@ -2,6 +2,11 @@ package com.ashkan.jira;
 
 import com.ashkan.jira.service.JiraService;
 import com.ashkan.jira.service.SprintReport;
+import com.ashkan.jira.sheets.SheetsUpdater;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +25,14 @@ public class ClientMain {
 	private void initialize() {
 //		jiraService.getDetailsForTicket();
 		sprintReport.generateSprintReport(923L);
+
+		final String spreadsheetId = "17guP0zy95DkAGKhEpExeNUyjMNB_BoJnNIy3yeothMU";
+		final SheetsUpdater updater = new SheetsUpdater(spreadsheetId);
+		try {
+			updater.updateSprintReport(sprintReport);
+		} catch (GeneralSecurityException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
